78ef5b1c4a07c593ab568c772ddef6ef3571b8cb
Eshller <20B030016@iitb.ac.in>
2023-06-29 01:10:05 +0530

    bug fix

--- src/pages/loginPage/login.jsx
+++ src/pages/loginPage/login.jsx
@@ -33,7 +33,7 @@ const LoginForm = () => {
     };
 
     // Send the login data to the API endpoint
-    axios.post('http://http://34.93.237.206:5005/api/users/login', loginData, { withCredentials: true })
+    axios.post('http://34.93.237.206:5005/api/users/login', loginData, { withCredentials: true })
       .then((response) => {
         // console.log("LOGIN DATA:", loginData);
         // console.log("api response here")
